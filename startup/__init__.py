07b51fcd40cc5da92c994f535a01f1dd5e76ea94
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-08-08 16:55:58 +0530

    Update __init__.py

--- startup/__init__.py
+++ startup/__init__.py
@@ -2,6 +2,14 @@
 import os
 from telethon import TelegramClient
 from telethon.session import StringSession
+from telethon import TelegramClient, events
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+import logging
+from pytgcalls import PyTgCalls
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
 
 # <-- Taking Configration -->
 API_ID = int(os.environ.get("API_ID", ""))
@@ -22,6 +30,7 @@ elif SESSION: # <-- if Session Given -->
     print("Found STring Session Activating user Clint")
     print("\n\n")
     print(".......")
+    call_py = PyTgCalls(app)
     app.run_until_disconnected()
 else: # <-- if not then you are Stupid -->
     print("Stupid Enter Bot Token or String session!")
