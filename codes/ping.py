37bd3802ca09327580eb3d23b9ea4a4be3598bb8
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-08-08 17:12:50 +0530

    Update ping.py

--- codes/ping.py
+++ codes/ping.py
@@ -2,21 +2,19 @@ import os
 
 from telethon import Button, events
 
-from Zaid import *
+from startup import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/5edf1b910c71e385e5d57.jpg"
+    "PING_PIC", "https://telegra.ph/file/8039084ae718d0eff674c.jpg"
 )
 ms = 4
 
-ALIVE = os.environ.get(
-    "ALIVE", "@Timesisnotwaiting"
 )
 
-CAPTION = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
+CAPTION = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『Telethon』"
 
 
-@Zaid.on(events.NewMessage(pattern="^/ping"))
+@bot.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/TheUpdatesChannel")]]
-    await Zaid.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
+    UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/Murat_30")]]
+    await bot.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
